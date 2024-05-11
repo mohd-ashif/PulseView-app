5229bae557e8de41a94a09f60031fb1257f5e219
mohd-ashif <mohdaszif077@gmail.com>
2024-05-11 11:14:41 +0530

    set up onboarding screen

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Home = () => {
   return (
     <View>
-      <Text>d</Text>
+      <Text>Home </Text>
     </View>
   )
 }
--- app/index.jsx
+++ app/index.jsx
@@ -1,27 +1,49 @@
 import { StatusBar } from "expo-status-bar";
-import { View, Text } from "react-native";
+import { View, Text, ScrollView ,Image } from "react-native";
 import { Link } from "expo-router";
+import { useFonts } from "expo-font";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Redirect, router } from "expo-router";
+import images from '../contants/images'
+import CustomButton from "../components/CustomButton";
 
 const App = () => {
-
-
-
 
   return (
     <>
 
-      <View className="w-full flex justify-center items-center h-full px-4">
+      <SafeAreaView className="bg-primary h-full">
+        <ScrollView contentContainerStyle={{height:' 100%'}}>
+            <View className="w-full justify-center items-center h-[85vh] px-4">
+            <Image source={images.logo} className="w-[130px] min-h-[84px]" resizeMode='contain' />  
 
-        <Text className="font-black text-3xl text-black-200 mt-7 text-center">
-        MotionWave
-        </Text>
+            <Image source={images.cards} className="max-w-[380px] w-full h-[300px]"  /> 
+            
+            <View className='relative mt-5'>
+              <Text className='text-3xl text-white font-bold text-center'>Discover Endless Possibilities  With {' '}
+               <Text className="text-secondary-200">Aora</Text></Text> 
+             <Image source={images.path} className='w-[136px] h-[15px] absolute -bottom-2 -right-7' resizeMode="contain" />
 
-        <StatusBar style="auto" />
+           
+            </View>
+            {/* <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+            Where Creativity Meets Innovation: Embark on a Journey of Limitless
+            Exploration with Aora
+          </Text> */}
 
-        <Link href="/home" style={{ color: 'blue' }}>  Go Home </Link>
-      </View>
+          <CustomButton
+            title="Continue with Email"
+            handlePress={() => router.push("/sign-in")}
+            containerStyles="w-full mt-7"
+          />
+            </View>
+        </ScrollView>
+
+
+       
+      </SafeAreaView>
     </>
-
+   
   );
 };
 
